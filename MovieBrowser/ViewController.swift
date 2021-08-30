222bd367de0a77287fe03217b3eea0564b64de1a
Tobias Schweiger <tschwei@siue.edu>
2021-08-30 15:39:54 -0500

    Initial Commit

---
+++ MovieBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Tobi Schweiger on 8/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
